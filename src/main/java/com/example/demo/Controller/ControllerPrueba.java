13c439fd75ac91df43c0e692dd5a3ca640c388d0
edyumg <eramirezc30@miumg.edu.gt>
2024-05-03 19:50:17 -0600

    docker

--- src/main/java/com/example/demo/Controller/ControllerPrueba.java
+++ src/main/java/com/example/demo/Controller/ControllerPrueba.java
@@ -12,7 +12,7 @@ public class ControllerPrueba {
 
     @GetMapping
     public ResponseEntity<String> GetPruebas(){
-        return ResponseEntity.ok("Esto es una prueba de Api rest con Spring Boot");
+        return ResponseEntity.ok("Esto es una prueba de Api rest con Spring Boot para progra 3");
 
     }
 }
